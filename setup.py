c0000c43f3930b447e44f91337ef6dc4e5fd2919
ozzy <ozzy@cydeo.com>
2024-06-01 11:17:48 -0400

    change package name

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.5"
 REPO_NAME = "package-creation"
-PKG_NAME= "database_automation"
+PKG_NAME= "database_automation_orcun"
 AUTHOR_USER_NAME = "orcuncanlilar"
 AUTHOR_EMAIL = "orcuncanlilar@gmail.com"
 
