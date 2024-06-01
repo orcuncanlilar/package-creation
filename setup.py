5330a9d93378a8096d4a96d330310d14e00a626e
ozzy <ozzy@cydeo.com>
2024-06-01 11:12:13 -0400

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 REPO_NAME = "package-creation"
 PKG_NAME= "database_automation"
 AUTHOR_USER_NAME = "orcuncanlilar"
